ac8adbc08e275589f7bc085348edd36167079e1b
had995 <gitara995@gmail.com>
2020-03-30 04:35:08 +0200

    postavljanje user interfacea

--- Main_app/Main_app/Main_app/main.c
+++ Main_app/Main_app/Main_app/main.c
@@ -19,9 +19,8 @@ float  distance, distance_red = 0;
 int16_t TimerOverflow = 0;
 int16_t distance_counter = 0;
 int16_t start_screen_flag = 1;
-int16_t selecting_screen_flag = 0;
-int16_t user1_flag = 0;
-int16_t user2_flag = 0;
+int16_t user1_flag = 0, distance_user1_exists = 0;
+int16_t user2_flag = 0, distance_user2_exists = 0;
 
 #define  Trigger_pin	PD5	/* Trigger pin, PD6 ECHO pin */ 
 #define  low_margin		510 /* low margin for value of adc which spins motors */
@@ -63,11 +62,13 @@ void save_distance(float distance, int user){
 	
 }
 
-void read_distance(int user){
+int read_distance(int user){
 	
-	int pozicija = 20 + user;
+	int pozicija = 30 + user;
 	
 	distance_red = eeprom_read_float((float *) pozicija);
+	
+	return distance_red;
 }
 
 
@@ -106,68 +107,73 @@ int main(void)
 		if(start_screen_flag){
 		
 		lcd_clrscr();
-		lcd_puts("Adjust hight!!!");
+		lcd_puts("USER1   USER2");
 		lcd_gotoxy(0,1);
-		lcd_puts("Key1 --> DONE");
+		lcd_puts("KEY1    KEY2");
 		
 		}
 		if(bit_is_clear(PINB,0)){
 			
-			selecting_screen_flag = 1;
+			user1_flag = 1; // user 1 is chosen
+			start_screen_flag = 0;
+		}else if(bit_is_clear(PINB,1)){
+			
+			user2_flag = 1;  // user 2 is chosen
 			start_screen_flag = 0;
 		}
 		
 		
-		if(selecting_screen_flag){
+		if(user1_flag && start_screen_flag == 0){
 			
-			lcd_clrscr();
-			lcd_puts("SELECT USER:");
-			lcd_gotoxy(0,1);
-			lcd_puts("USER1");
-			lcd_gotoxy(7,1);
-			lcd_puts("USER2");
-
-		}
-		if(bit_is_clear(PINB,0) && selecting_screen_flag){ // user1
-			
-			save_distance(distance,1); /* saving distance(hight) for user, calculating average */
-			
-			selecting_screen_flag = 0;
-			
-			user1_flag = 1;
-			
-		}
-		if(bit_is_clear(PINB,1) && selecting_screen_flag){ // user2
-			
-			//save_distance(distance,2);
-			
-			selecting_screen_flag = 0;
-			
-			user2_flag = 1;
-			
-		}
-		
-		if(selecting_screen_flag == 0 && user1_flag){
+			if(read_distance(1) != 0){
 				
-				read_distance(1);
+				distance = read_distance(1); 
+				
+				// if hight already exists, set the hight of a table to this
+				// also starting position( sitting standing should be automatically determined 
+				// depending of a hight
+				// average sitting table hight is 75 cm, so everything under 100 should be sitting
+				
+				distance_user1_exists = 1;
+			}
+			else{
+				
 				lcd_clrscr();
-				lcd_puts("USER1 SJEDECA");
+				lcd_puts("POSTAVITE VISINU");
 				lcd_gotoxy(0,1);
-				/* todo : using calculated distance to automatically set hight */
-				/* using keys 3 and 4 to switch between sitting and standing */
-				distance_to_string(distance_red);
+				lcd_puts("KEY1 ---> DONE");
 				
+				if(bit_is_clear(PINB,1)){
+					
+					save_distance(mesure_distance(),1); /* saving distance(hight) for user, calculating average */
+					
+					// standing position ???
 				
-		}
-		if(selecting_screen_flag == 0 && user2_flag){
+				//ok, problem je da on pristupi ovoj if petlji iako nesmije, zašto? a niti ne spremi udaljenost
 				
-				lcd_clrscr();
-				lcd_puts("USER2 SJEDECA");
-			    lcd_gotoxy(0,1);
-				distance_to_string(distance);
-		}
-		
-		
+				}
+			}
+				
+				if(distance_user1_exists){
+					
+					lcd_clrscr();
+					lcd_puts("STOJECA POVRATAK:");
+					lcd_gotoxy(0,1);
+					lcd_puts("KEY1    KEY2");
+					
+					if(bit_is_clear(PINB,0)){
+						
+						// set a logic to changing to a standing position
+						// mby 2 memory spaces for each user
+						
+					}
+					if(bit_is_clear(PINB,1)){
+						
+						start_screen_flag = 1; // return
+					}	
+				}
+			}
+			
 		distance_counter++; 
 		
 		_delay_ms(10);
